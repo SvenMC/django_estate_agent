0b61e2ffbaf5ecf6c8727de58ffb8c624c2d6c15
SvenMC <svencooke7@gmail.com>
2023-07-17 20:30:45 +0100

    add property_detail redirect

--- estate_agent/estate_agent/views.py
+++ estate_agent/estate_agent/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return HttpResponse('<h3>Index Page</h3>')
@@ -13,6 +13,7 @@ def for_sale_index(request):
 def property_detail(request):
     return HttpResponse('<h3>property_detail Page</h3>')
 
+@login_required(login_url="/admin/login/")
 def property_form(request):
     return HttpResponse('<h3>property_form Page</h3>')
 
