91945616d0cc2395abb18bb10e73875b16f3dddf
SvenMC <svencooke7@gmail.com>
2023-07-17 20:46:11 +0100

    render html templates

--- estate_agent/estate_agent/views.py
+++ estate_agent/estate_agent/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h3>Index Page</h3>')
+    return render(request, 'index.html')
 
 def for_rent_index(request):
-    return HttpResponse('<h3>for_rent_index Page</h3>')
+    return render(request, 'for_rent_index.html')
 
 def for_sale_index(request):
-    return HttpResponse('<h3>for_sale_index Page</h3>')
+    return render(request, 'for_sale_index.html')
 
 def property_detail(request):
-    return HttpResponse('<h3>property_detail Page</h3>')
+    return render(request, 'property_detail.html')
 
 @login_required(login_url="/admin/login/")
 def property_form(request):
-    return HttpResponse('<h3>property_form Page</h3>')
+    return render(request, 'property_form.html')
 
 def login_portal(request):
-    return HttpResponse('<h3>login_portal Page</h3>')
+    return render(request, 'login_portal.html')
