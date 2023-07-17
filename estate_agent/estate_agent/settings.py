1117c9859312399bd81c9d9339e36e151a6299f9
SvenMC <svencooke7@gmail.com>
2023-07-17 22:12:52 +0100

    add secure key

--- estate_agent/estate_agent/settings.py
+++ estate_agent/estate_agent/settings.py
@@ -12,16 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load env vars
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gv1u3s2cryc3du=-zo(ho=1u^8s)@+z+h9v%$!f7p$)*$mzdw&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
