e3cf55ba6c24a9463ac9432411507fe8fe7fd11f
SvenMC <svencooke7@gmail.com>
2023-08-27 19:15:03 +0100

    add django filters to Properties endpoint

--- estate_agent/estate_agent/settings.py
+++ estate_agent/estate_agent/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'estate_agent',
 ]
@@ -110,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # REST Framework
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
--- estate_agent/estate_agent/views.py
+++ estate_agent/estate_agent/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 from rest_framework import viewsets
 from rest_framework import permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Property, PropertyImage
 from .forms import PropertyForm, PropertyImageForm
@@ -26,11 +27,13 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class PropertiesViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Properties to be viewed or edited.
     """
     queryset = Property.objects.all().order_by('-last_updated')
     serializer_class = PropertiesSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['contract_type']
 
 
 def index(request: HttpRequest):
