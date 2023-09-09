ae63b18fd76df98cc60a01b8b3ce7fd3d37abbf1
SvenMC <svencooke7@gmail.com>
2023-09-09 13:02:07 +0100

    test property request

--- frontend/src/Pages/property.tsx
+++ frontend/src/Pages/property.tsx
@@ -1,6 +1,20 @@
+import axios, * as others from 'axios';
+
+function getProperty(): string {
+
+  const url = "http://127.0.0.1:8000/api/properties/12/";
+  const response = axios.get(url);
+  var text: string = response.toString();
+  return text;
+
+}
+
+
 function Property() {
+  const property = getProperty();
   return (
     <div className="Property">
+      <h1>{property}</h1>
     </div>
   );
 }
