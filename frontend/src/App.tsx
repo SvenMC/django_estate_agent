28ea495f0932d9f74ca34272bf88635b03030709
SvenMC <svencooke7@gmail.com>
2023-09-10 12:34:05 +0100

    add homepage and route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Property from './Pages/Property';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/property/:id/" element={<Property />}>
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                </Route>
+                <Route path="/property/:id/" element={<Property />}>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
