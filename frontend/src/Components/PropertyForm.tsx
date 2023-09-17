2410bacfee5173f9734148856b8cd6f3800cb92c
SvenMC <svencooke7@gmail.com>
2023-09-17 16:45:24 +0100

    temporarily disable unused fields

--- frontend/src/Components/PropertyForm.tsx
+++ frontend/src/Components/PropertyForm.tsx
@@ -81,6 +81,7 @@ export default function PropertyForm() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Unused"
                   required={false}
+                  disabled={true}
                 ></input>
               </div>
               <div className="w-full">
@@ -97,6 +98,7 @@ export default function PropertyForm() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="$2999"
                   required={false}
+                  disabled={true}
                 ></input>
               </div>
               <div>
@@ -129,6 +131,7 @@ export default function PropertyForm() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="12"
                   required={false}
+                  disabled={true}
                 ></input>
               </div>
               <div className="sm:col-span-2">
