824c00469fdeae63519b60acca6a06bcb8b481e7
SvenMC <svencooke7@gmail.com>
2023-09-17 12:50:27 +0100

    read form on submit

--- frontend/src/Components/PropertyForm.tsx
+++ frontend/src/Components/PropertyForm.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+interface PropertyFormElements extends HTMLFormControlsCollection {
+    address: HTMLInputElement
+    contract: HTMLInputElement
+    description: HTMLInputElement
+}
 
+interface PropertyFormElement extends HTMLFormElement {
+   readonly elements: PropertyFormElements
+}
 export default function PropertyForm() {
+
+    function handleSubmit(event: React.FormEvent<PropertyFormElement>) {
+        event.preventDefault();
+        console.log(event.currentTarget.elements.address.value);
+        console.log(event.currentTarget.elements.contract.value);
+        console.log(event.currentTarget.elements.description.value);
+    }
+
   return (
     <div className="PropertyForm py-8">
       <section className="bg-white dark:bg-gray-800 rounded-3xl shadow-2xl">
@@ -8,7 +24,7 @@ export default function PropertyForm() {
           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
             Add a new property
           </h2>
-          <form action="#">
+          <form onSubmit={handleSubmit}>
             <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
               <div className="sm:col-span-2">
                 <label
@@ -69,7 +85,7 @@ export default function PropertyForm() {
                   id="contract"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 >
-                <option disabled selected>Select contract</option>
+
                   <option value="Rent">Rent</option>
                   <option value="Sale">Sale</option>
                 </select>
@@ -102,6 +118,7 @@ export default function PropertyForm() {
                   rows={8}
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Your property description here"
+                  required={true}
                 ></textarea>
               </div>
             </div>
