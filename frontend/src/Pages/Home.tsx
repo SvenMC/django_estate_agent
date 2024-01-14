de11ea71251917bf9b3f9cab0d07bdc7fe985d5d
SvenMC <svencooke7@gmail.com>
2024-01-14 19:46:58 +0000

    add hidden key parsing via config

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import config from "../config";
 
 export default function Home() {
+
+  console.log(config.mapApiKey)
   return (
     <div className="Home">
       <h1 className="text-4xl">Home page</h1>
--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,3 +1,6 @@
-const base_api: string = "http://127.0.0.1:8000/";
+const config = {
+    baseApiUrl: "http://127.0.0.1:8000/",
+    mapApiKey: `${process.env.REACT_APP_TEST_MAP_KEY}`,
+  };
 
-export default base_api;
+export default config;
