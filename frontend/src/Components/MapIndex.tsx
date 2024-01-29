8813d83c8de466f14d4bec3422f3a525ed6e1ccd
SvenMC <svencooke7@gmail.com>
2024-01-29 14:07:51 +0000

    add working map markers

--- frontend/src/Components/MapIndex.tsx
+++ frontend/src/Components/MapIndex.tsx
@@ -1,31 +1,31 @@
 import React, { useEffect } from "react";
 import config from "../config";
 import maplibregl from 'maplibre-gl';
+import '../Styles/MapLibre.css';
 
 export default function MapIndex() {
     useEffect(() => {
         const map = new maplibregl.Map({
-          container: 'map', // container id
-          style: 'https://tiles.stadiamaps.com/styles/osm_bright.json', // style URL
-          center: [-0.33769, 53.74515], // starting position [lng, lat]
-          zoom: 13, // starting zoom
-          maxZoom: 14,
-          minZoom: 12
-      });
-    
+            container: 'map', // container id
+            style: 'https://tiles.stadiamaps.com/styles/osm_bright.json', // style URL
+            center: [-0.33769, 53.74515], // starting position [lng, lat]
+            zoom: 13, // starting zoom
+            maxZoom: 14,
+            minZoom: 12
+        });
+
         const el = document.createElement('div');
-        el.className = 'house-icon'; 
+        el.className = 'house-icon';
         el.innerHTML = `<img src="${config.mapHouseIconUrl}" alt="House" />`;
-    
+
         const marker = new maplibregl.Marker({
-          element: el,
-          draggable: false,
-          scale: 1  ,
-          }).setLngLat([-0.33769, 53.74515])
-          .addTo(map);
-    
-      }, []);
-    return(
-        <div id='map' style={{width: '100%', height: '1000px'}}></div>
+            color: "#FFFFFF",
+            element: el,
+            }).setLngLat([-0.3377063645579368, 53.74514666367165])
+            .addTo(map);
+
+    }, []);
+    return (
+        <div id='map' style={{ width: '100%', height: '1000px' }}></div>
     )
 }
--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,7 +1,7 @@
 const config = {
     baseApiUrl: "http://127.0.0.1:8000/",
     mapApiKey: `${process.env.REACT_APP_TEST_MAP_KEY}`,
-    mapHouseIconUrl: "https://f003.backblazeb2.com/file/django-estate-dev/static/home_icon.png"
+    mapHouseIconUrl: "https://f003.backblazeb2.com/file/django-estate-dev/static/house_pin.png"
   };
 
 export default config;
